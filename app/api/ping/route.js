88aa6bd261605d4ee0f6f56b78c9f8937eefdff4
oezdhakan <oezdhakan@googlemail.com>
2025-08-13 19:50:56 +0200

    Delete app/api/ping/route.js

--- app/api/ping/route.js
+++
@@ -1,6 +0,0 @@
-export const runtime = "nodejs";
-export const dynamic = "force-dynamic";
-
-export async function GET() {
-  return Response.json({ ok: true, route: "ping", time: new Date().toISOString() });
-}
